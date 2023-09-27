835128bb40228e378e6f633773368eb95c1d1bac
gabrielzitto <gos2@cin.ufpe.br>
2023-09-27 14:55:06 -0300

    consultas_gab

---
+++ consultas.sql
@@ -0,0 +1,82 @@
+
+
+-- selecionar todos os dados de uma tabela   
+
+SELECT * FROM Perfil;
+
+-- --------------------------------------------------------------------------------------------------
+
+
+-- Realizar consulta de seleção-projeção-junção
+SELECT P.nome, P.sobrenome, U.data_assinatura
+FROM Perfil P
+INNER JOIN Usuario U ON P.email = U.email_usuario
+WHERE U.data_assinatura >= TO_DATE('2023-09-25', 'YYYY-MM-DD');
+-- --------------------------------------------------------------------------------------------------
+
+
+-- utilizar função de agregação
+
+SELECT P.id, P.titulo_da_postagem, SUM(C.num_curtidas) AS total_curtidas
+FROM Postagem P
+LEFT JOIN Associa1 A1 ON P.id = A1.postagem
+LEFT JOIN Comentario C ON A1.comentario = C.id
+WHERE P.id = 1
+GROUP BY P.id, P.titulo_da_postagem;
+
+-- --------------------------------------------------------------------------------------------------
+
+-- Criar regra para execução de comandos DDL
+CREATE OR REPLACE PROCEDURE ddl_exec_rule AS
+BEGIN
+  IF (ora_dict_obj_type IN ('TABLE', 'VIEW', 'SEQUENCE', 'PROCEDURE', 'FUNCTION', 'TRIGGER')) THEN
+    RAISE_APPLICATION_ERROR(-20001, 'Ação não permitida: Comandos DDL não são permitidos neste contexto.');
+  END IF;
+END;
+/
+-- --------------------------------------------------------------------------------------------------
+
+
+-- criar usuário que possua a regra padrão para comandos DDL (criada anteriormente)
+-- Crie um usuário com a regra padrão para comandos DDL
+CREATE USER seu_usuario IDENTIFIED BY sua_senha;
+
+-- Atribua a regra para comandos DDL ao usuário
+GRANT EXECUTE ON ddl_exec_rule TO seu_usuario;
+
+-- Defina a regra como padrão para comandos DDL para o usuário
+ALTER USER seu_usuario DEFAULT ROLE ddl_exec_rule;
+
+-- Conceda privilégios adicionais ao usuário, conforme necessário
+-- Por exemplo:
+-- GRANT CONNECT, RESOURCE TO seu_usuario;
+
+-- --------------------------------------------------------------------------------------------------
+
+
+-- criar uma transação única
+
+DECLARE
+  v_comentario_id VARCHAR2(10) := '1';
+  v_incremento INT := 1;
+BEGIN
+  -- Iniciar a transação
+  SAVEPOINT inicio_transacao;
+
+  -- Atualizar o número de curtidas no comentário
+  UPDATE Comentario
+  SET num_curtidas = num_curtidas + v_incremento
+  WHERE id = v_comentario_id;
+
+  -- Se a atualização for bem-sucedida, confirmar a transação
+  COMMIT;
+  
+  DBMS_OUTPUT.PUT_LINE('Transação concluída com sucesso!');
+EXCEPTION
+  WHEN OTHERS THEN
+    -- Em caso de erro, desfazer a transação
+    ROLLBACK TO inicio_transacao;
+    DBMS_OUTPUT.PUT_LINE('Erro durante a transação. Transação desfeita.');
+END;
+/
+
