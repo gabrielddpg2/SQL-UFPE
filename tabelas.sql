05918065b11f85d50216f5a696e57d4e8a3cb7e8
gabrielzitto <gos2@cin.ufpe.br>
2023-10-02 14:51:20 -0300

    check_idade

--- tabelas.sql
+++ tabelas.sql
@@ -20,7 +20,7 @@ CREATE TABLE Perfil (
   email VARCHAR2(60) NOT NULL,
   nome VARCHAR2(255) NOT NULL,
   sobrenome VARCHAR2(255) NOT NULL,
-  data_nascimento DATE CHECK (EXTRACT(YEAR FROM data_nascimento) >= 1915) NOT NULL,
+  data_nascimento DATE CHECK (EXTRACT(YEAR FROM data_nascimento) >= 1900) NOT NULL,
   data_cadastro DATE NOT NULL,
   idade INT,
 
